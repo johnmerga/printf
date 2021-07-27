3d717aec7ffe5685392d87921300af1e447b2505
Osatohanmwen Nosa-Ojo <elanfrances3@gmail.com>
2021-07-27 08:08:24 -0700

    characters

--- print_chars.c
+++ print_chars.c
@@ -9,10 +9,10 @@
  */
 int print_c(va_list c)
 {
-        char ch = (char)va_arg(c, int);
+	char ch = (char)va_arg(c, int);
 
-        _putchar(ch);
-        return (1);
+	_putchar(ch);
+	return (1);
 }
 
 /**
@@ -23,16 +23,16 @@ int print_c(va_list c)
  */
 int print_s(va_list s)
 {
-        int count;
-        char *str = va_arg(s, char *);
+	int count;
+	char *str = va_arg(s, char *);
 
-        if (str == NULL)
-                str = "(null)";
-        for (count = 0; str[count]; count++)
-        {
-                _putchar(str[count]);
-        }
-        return (count);
+	if (str == NULL)
+		str = "(null)";
+	for (count = 0; str[count]; count++)
+	{
+		_putchar(str[count]);
+	}
+	return (count);
 }
 
 /**
